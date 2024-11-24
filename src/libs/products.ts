fc5e3e6fedb615c0ac8344fbfb011ee852e291d9
Fabien <lapertfabien@gmail.com>
2024-11-24 21:56:58 +0100

    feature(product): add single product page

--- src/libs/products.ts
+++ src/libs/products.ts
@@ -1,24 +1,28 @@
-import {products} from "@/data/products";
+import { products } from "@/data/products";
 
 export type Product = {
-    id: number,
-    category: string,
-    name: string,
-    price: number,
-    sizes: string[],
-    material: string,
-    colors: string[],
-    features: string[],
-    images?: string[],
-}
+  id: number;
+  category: string;
+  name: string;
+  price: number;
+  sizes: string[];
+  material: string;
+  colors: string[];
+  features: string[];
+  images?: string[];
+};
 
 export type ProductFilter = {
-    categories: string[],
-    sizes: string[],
-    colors: string[],
-    materials: string[],
-}
+  categories: string[];
+  sizes: string[];
+  colors: string[];
+  materials: string[];
+};
 
 export function getProducts() {
-    return products;
+  return products;
+}
+
+export function getProductById(id: number) {
+  return products.find((p) => p.id === id) || null;
 }
